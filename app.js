1a38d5b38a4bdc8298a850c9da6acf90328045f7
GormTheWyrm <gdsoucie@gmail.com>
2020-04-28 16:09:30 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+//mysql
+//console tabel?
+
+// variable declarations
+
+//need to learn waht a join is
+//3 major tables in 1 database; department, role, employee
+
+
+
+// Functions
+
+
+/* **** Main App Here **** */
+
+//call start
+//might try making it a do while loop, ending function by turning a isOn variable to false
