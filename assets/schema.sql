6c9b357cf82c48b4f8f7db0f5147f9ff6d8a6845
GormTheWyrm <gdsoucie@gmail.com>
2020-05-06 18:56:28 -0400

    pushed temporary gif

--- assets/schema.sql
+++ assets/schema.sql
@@ -3,6 +3,7 @@ SELECT * FROM employee;
 SELECT * FROM job_role;
 -- "role" will cause errors in the future as it is likely to become a reserved keyword
 -- therefore I changed it to job_role
+USE open_employee_db;
 
 UPDATE employee SET first_name = "Jim" WHERE id = 1;
 
@@ -17,9 +18,14 @@ FROM employee
 LEFT JOIN job_role
 ON employee.role_id = job_role.id
 LEFT JOIN department
-ON job_role.department_id = department.department_id;
-
--- view department
-
+ON job_role.department_id = department.id;
 
 -- view role
+-- want to see the dept name in this
+SELECT job_role.title, job_role.salary, department.name
+FROM job_role
+LEFT JOIN department.name
+ON job_role.department_id = department.id;
+
+-- view department
+-- skipping any joins for department
